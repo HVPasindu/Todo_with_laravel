cad80162c53cc99e8d66fbdd629dd4ac5f1af1e1
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-05-16 00:09:45 +0530

    Add columns to todos table: name, address, and done status

--- database/migrations/2026_05_15_170656_create_todos_table.php
+++ database/migrations/2026_05_15_170656_create_todos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->integer('done')->default(0);
             $table->timestamps();
         });
     }
