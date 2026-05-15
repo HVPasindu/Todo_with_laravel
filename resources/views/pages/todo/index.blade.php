294e7e2bdb61bdad7fa7f8d4d29fff1fe84dd93f
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-05-16 04:46:18 +0530

    Create data-save data

--- resources/views/pages/todo/index.blade.php
+++ resources/views/pages/todo/index.blade.php
@@ -8,17 +8,40 @@
                 <h1 class="page-title">Todo Page</h1>
             </div>
 
+
+
         </div>
+
+
     </div>
+
+    <form class="ms-5 mt-4" style="max-width: 600px;" action="{{ route('todo.store') }}" method="POST" enctype="multipart/form">
+        @csrf
+        <div class="row mb-3 align-items-center">
+            <label for="name" class="col-sm-3 col-form-label">Name</label>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" id="name" name="name">
+            </div>
+        </div>
+
+        <div class="row mb-3 align-items-center">
+            <label for="address" class="col-sm-3 col-form-label">Address</label>
+            <div class="col-sm-9">
+                <input type="text" class="form-control" id="address" name="address">
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
 @endsection
 
 @push('css')
-<style>
-    .page-title{
-        padding-top: 5vh;
-        color: #65c908;
-        font-size: 5rem
-    }
-</style>
-
+    <style>
+        .page-title {
+            padding-top: 5vh;
+            color: #65c908;
+            font-size: 5rem
+        }
+    </style>
 @endpush
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TodoController;
 
-//Home 
+//Home
 Route::get('/',[HomeController::class,"index"])->name('home');
 
 //Todo
-Route::get('/todo',[TodoController::class,"index"])->name('todo');
+
+
+Route::prefix('/todo')->group(function (){
+    Route::get('/',[TodoController::class,"index"])->name('todo');
+    Route::post('/store',[TodoController::class,"store"])->name('todo.store');
+});
